c2f647f5eff045b3779c29129c9b522b97d0d283
heybyte <joagchim@msn.com>
2018-05-25 21:17:33 +0200

    First upload

---
+++ dev-debugger.php
@@ -0,0 +1,11 @@
+<?php
+
+	/**
+	 * Plugin Name: Dev Debugger
+	 * Plugin URI: https://heybyte.github.io/wp-dev-debugger
+	 * Description: Peek under the hood with sixty debugging reports just one click away.
+	 * Version: 0.1
+	 * Author: Joachim Maquelin
+	 * Author URI: https://github.com/heybyte
+	 * License: GPLv3 or Later
+	 */
